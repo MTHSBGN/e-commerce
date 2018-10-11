eaab7ec3bb07307e2c48dacf18d780352c85b114
Mathias Beguin <mathias.beguin@hotmail.com>
2018-10-11 17:31:30 +0200

    First version of login done

--- dist/routes/login.js
+++ dist/routes/login.js
@@ -4,8 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const router = express_1.default.Router();
 router.get('/login', (req, res) => {
-    res.send('Login page');
+    res.sendFile(path_1.default.join(__dirname, '../public/html', 'login.html'));
+});
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    if (req.body.username === 'admin' && req.body.password === 'admin') {
+        console.log('redirect');
+        res.redirect('/');
+    }
+    else {
+        res.send({ message: 'Invalid credentials' });
+    }
 });
 exports.default = router;
