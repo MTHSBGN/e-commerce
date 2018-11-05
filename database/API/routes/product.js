7e44d73cd64f4d67a9f96d465a72980af3e979ee
Mathias Beguin <mathias.beguin@hotmail.com>
2018-11-05 19:13:44 +0100

    Routes for the product table done

--- database/API/routes/product.js
+++ database/API/routes/product.js
@@ -3,12 +3,25 @@ const connection = require('../database');
 
 const router = express.Router();
 
+// Returns all the products of the database
 router.get('/', (req, res) => {
-  connection.query('SELECT * FROM Customer', function(err, rows) {
+  connection.query('SELECT * FROM Product', function(err, rows) {
     if (err) throw err;
 
     res.send(rows);
   });
+});
+
+// Returns specific product
+router.get('/:productID', (req, res) => {
+  connection.query(
+    `SELECT * FROM Product WHERE product_id = ${req.params.productID}`,
+    function(err, rows) {
+      if (err) throw err;
+
+      res.send(rows);
+    }
+  );
 });
 
 module.exports = router;
