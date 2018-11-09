4e27d55e8459d83dfd921c8314f64db062398ed8
Mathias Beguin <mathias.beguin@hotmail.com>
2018-11-09 18:33:04 +0100

    Added image in public directory

--- database/API/routes/home.js
+++ database/API/routes/home.js
@@ -5,14 +5,9 @@ const router = express.Router();
 
 router.get('/', function(req, res) {
   database.connection.query(
-    'SELECT * FROM Product INNER JOIN Sku ON Product.product_id=Sku.product_id INNER JOIN Variant ON Sku.sku_id=Variant.sku_id WHERE Variant.attribute = "images" GROUP BY Product.product_id',
+    'SELECT Product.name, Product.main_image, Sku.price FROM Product INNER JOIN Sku ON Product.product_id = Sku.product_id',
     (err, rows) => {
       if (err) throw err;
-      for (product of rows) {
-        // console.log(JSON.parse(product.value));
-        product.image = JSON.parse(product.value).images[0];
-      }
-      console.log(rows[0]);
       res.render('home', {
         products: rows
       });
