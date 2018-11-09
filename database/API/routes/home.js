0af07f7a56a55f933b275ca90ff180bea58dd626
Mathias Beguin <mathias.beguin@hotmail.com>
2018-11-09 17:44:17 +0100

    Added products cards

--- database/API/routes/home.js
+++ database/API/routes/home.js
@@ -8,7 +8,11 @@ router.get('/', function(req, res) {
     'SELECT * FROM Product INNER JOIN Sku ON Product.product_id=Sku.product_id INNER JOIN Variant ON Sku.sku_id=Variant.sku_id WHERE Variant.attribute = "images" GROUP BY Product.product_id',
     (err, rows) => {
       if (err) throw err;
-
+      for (product of rows) {
+        // console.log(JSON.parse(product.value));
+        product.image = JSON.parse(product.value).images[0];
+      }
+      console.log(rows[0]);
       res.render('home', {
         products: rows
       });
